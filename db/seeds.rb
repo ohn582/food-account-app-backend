d26da82cbd8ee8100501a965a8d39003a7c6546b
Neal <no4259@gmail.com>
2020-09-07 17:11:28 -0400

    backend changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ========================== {Seeds} ==========================
+
+User.destroy_all
+Product.destroy_all
+Cart.destroy_all
+CartItem.destroy_all
+
+
+
+# ========================== {User} ==========================
+
+User.create(username: "Ali", password: "1234", admin: true)
+User.create(username: "Jamalino", password: "1234", admin: false)
+User.create(username: "Jamackino", password: "1234", admin: false)
+
+
+# ========================== {Product} =======================
+
+
+Product.create(name:"Allo La Vie",description: "In the mid-1980s, two aspiring Italian designers founded what would later become one of the country’s most recognizable luxury fashion houses. Seven years later, Dolce and Gabbana presented their first menswear collection and have been creating lust-worthy suits ever since." , price: 320, image:"https://assets.overland.com/is/image/overlandsheepskin/16146-btcg-av90249?$style%2Dlg%2D2x$&id=GlIsL0&fmt=jpg&fit=constrain,1")
+Product.create(name:"Bravo Suit",description: "In the mid-1980s, two aspiring Italian designers founded what would later become one of the country’s most recognizable luxury fashion houses. Seven years later, Dolce and Gabbana presented their first menswear collection and have been creating lust-worthy suits ever since." , price: 250, image:"https://www.luxois.com/photos/articles/2163.6691.jpg")
+Product.create(name:"Sauver Jacket",description: "In the mid-1980s, two aspiring Italian designers founded what would later become one of the country’s most recognizable luxury fashion houses. Seven years later, Dolce and Gabbana presented their first menswear collection and have been creating lust-worthy suits ever since." , price: 320, image:"https://assets.overland.com/is/image/overlandsheepskin/26421-jscg-av0143?$style%2Dlg%2D2x$&id=GlIsL0&fmt=jpg&fit=constrain,1")
+Product.create(name:"Max Made",description: "In the mid-1980s, two aspiring Italian designers founded what would later become one of the country’s most recognizable luxury fashion houses. Seven years later, Dolce and Gabbana presented their first menswear collection and have been creating lust-worthy suits ever since." , price: 120, image:"https://assets.overland.com/is/image/overlandsheepskin/17189-brb2-av90626?$style%2Dlg%2D2x$&id=GlIsL0&fmt=jpg&fit=constrain,1")
+
+
+# ========================== {Cart} =========================
+
+
+
+
+
+
+
+
+puts "Seeded Successfully "
