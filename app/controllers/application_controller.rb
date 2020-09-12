c56932a4bdcb324250003bd5aaa9733b21241796
Neal <no4259@gmail.com>
2020-09-11 20:00:01 -0400

    backend update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,5 +27,4 @@ class ApplicationController < ActionController::API
     def require_login
       render json: {error: 'Unauthorized'}, status: :unauthorized if !client_has_valid_token?
     end
-    1
 end
